b17585ff0540ebb4bb8b9f7cf529feaed9f46fba
LilianaCondrea <l.cndrea@yahoo.com>
2021-10-16 16:42:25 +0300

    light/dark mode

---
+++ script.js
@@ -0,0 +1,56 @@
+const toggleSwitch=document.querySelector('input');
+const nav=document.getElementById('nav');
+const toggleIcon=document.getElementById('toggle-icon');
+const image1=document.getElementById('image1');
+const image2=document.getElementById('image2');
+const image3=document.getElementById('image3');
+const textBox=document.getElementById('text-box');
+
+// Dark or light images
+function imageMode(color){
+    image1.src = `img/undraw_proud_coder_${color}.svg`;
+    image2.src = `img/undraw_feeling_proud_${color}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
+
+// Dark Mode
+function darkMode(){
+    nav.style.backgroundColor='rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor='rgb(255,255,255 / 50%)';
+    toggleIcon.children[0].textContent='Dark Mode';
+    toggleIcon.children[1].classList.replace('fa-sun','fa-moon');
+    imageMode('dark');
+}
+// Dark Mode
+function lightMode(){
+    nav.style.backgroundColor='rgb(255,255,255 / 50%)';
+    textBox.style.backgroundColor='rgb(0 0 0/ 50%)';
+    toggleIcon.children[0].textContent='Light Mode';
+    toggleIcon.children[1].classList.replace('fa-moon','fa-sun');
+    imageMode('light');
+}
+// Switch Theme Dynamically
+function switchTheme(event){
+    if(event.target.checked){
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme','dark');
+        darkMode();
+    }else{
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme','light');
+        lightMode();
+    }
+}
+
+// Event listener
+toggleSwitch.addEventListener('change',switchTheme);
+
+// check local storage for theme
+const currentTheme=localStorage.getItem('theme');
+if(currentTheme){
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if(currentTheme === 'dark'){
+        toggleSwitch.checked=true;
+        darkMode();
+    }
+}
